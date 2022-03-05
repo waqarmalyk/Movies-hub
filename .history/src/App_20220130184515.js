f813b223082928968456f2b88300fc6fb684df02
Waqar Malik <admin@WaqarMalyk.local>
2022-03-05 17:13:39 +0100

    Added my project

---
+++ .history/src/App_20220130184515.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+  const [location, setLocation] = useState({
+    
+
+  })
+
+  return (
+    <div className="App">
+      <header className="App-header">General App</header>
+    </div>
+  );
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,5 @@
+# Movies-hub
+
+## Search movie with name string
+
+### Displaying movies in the dom.
